140f88068535e35b463ceddefdd45a2e51b005a0
siso9 <siso95669@gmail.com>
2024-11-27 14:57:57 +0200

    Added new Class seed

--- FitnessCenter.Data/Configuration/ClassConfiguration.cs
+++ FitnessCenter.Data/Configuration/ClassConfiguration.cs
@@ -39,16 +39,23 @@ namespace FitnessCenter.Data.Configuration
                 new Class()
                 {
                     Title = "Yoga Class",
-                    StartingDate = new DateTime(2024, 12, 03),
+                    StartingDate = new DateTime(2024, 12, 16, 12, 00, 00),
                     Duration = 90,
                     Description = "Perfect for beginners or those seeking a calming, slower-paced practice. This class focuses on foundational poses, gentle stretches, and breathwork to enhance flexibility and relaxation. No prior experience needed."
                 },
                 new Class()
                 {
                     Title = "Full-Body Strength Training",
-                    StartingDate = new DateTime(2024, 12, 04),
-                    Duration = 120,
+                    StartingDate = new DateTime(2024, 12, 13, 11, 00, 00),
+                    Duration = 70,
                     Description = "A well-rounded workout targeting all major muscle groups. Incorporates free weights, resistance machines, and bodyweight exercises to improve overall strength, endurance, and stability. Suitable for all levels, with modifications available."
+                },
+                new Class()
+                {
+                    Title = "Basketball Training",
+                    StartingDate = new DateTime(2024, 12, 13, 16, 00, 00),
+                    Duration = 120,
+                    Description = "A basketball training program is a specialized practice designed to improve an individual's skillset. It typically involves drills and exercises focused on developing specific areas, such as ball handling, shooting, passing, and agility."
                 }
             };
 
