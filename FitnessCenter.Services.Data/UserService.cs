6d69b66844ad1c6c4828bb057e8c07640cb9eca4
siso9 <siso95669@gmail.com>
2024-11-28 14:20:21 +0200

    Create UserService for managing Users and Roles in the WebApp

---
+++ FitnessCenter.Services.Data/UserService.cs
@@ -0,0 +1,33 @@
+﻿using FitnessCenter.Services.Data.Interfaces;
+using FitnessCenter.Web.ViewModels.Admin.UserManagement;
+
+namespace FitnessCenter.Services.Data
+{
+    public class UserService : BaseService, IUserService
+    {
+        public Task<bool> AssignUserToRoleAsync(Guid userId, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUserAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<AllUsersViewModel>> GetAllUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveUserRoleAsync(Guid userId, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExistsByIdAsync(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
