2a5f1f1ccbcb0f598991eaee22e30d4e3eca7669
siso9 <siso95669@gmail.com>
2024-11-28 15:31:41 +0200

    Added the Subscribtion Interface and service

---
+++ FitnessCenter.Services.Data/Interfaces/ISubscribtionService.cs
@@ -0,0 +1,8 @@
+﻿namespace FitnessCenter.Services.Data.Interfaces
+{
+    using FitnessCenter.Web.ViewModels.GymClass;
+    public interface ISubscribtionService
+    {
+        public Task<bool> SetAvailableTicketsAsync(SetAvailableSubscribtionsViewModel model);
+    }
+}
---
+++ FitnessCenter.Services.Data/SubscribtionService.cs
@@ -0,0 +1,26 @@
+﻿using FitnessCenter.Data.Models;
+using FitnessCenter.Data.Repository.Interfaces;
+using FitnessCenter.Services.Data.Interfaces;
+using FitnessCenter.Services.Mapping;
+using FitnessCenter.Web.ViewModels.GymClass;
+
+namespace FitnessCenter.Services.Data
+{
+    public class SubscribtionService : BaseService, ISubscribtionService
+    {
+        private readonly IRepository<GymClass, object> gymClassRepository;
+
+        public SubscribtionService(IRepository<GymClass, object> gymClassRepository)
+        {
+            this.gymClassRepository = gymClassRepository;
+        }
+
+        public async Task<bool> SetAvailableTicketsAsync(SetAvailableSubscribtionsViewModel model)
+        {
+            GymClass gymClass = AutoMapperConfig.MapperInstance
+                .Map<GymClass>(model);
+
+            return await this.gymClassRepository.UpdateAsync(gymClass);
+        }
+    }
+}
